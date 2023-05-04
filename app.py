ebbf0329521323fa603710de5412d1ad15f44705
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-05-05 01:52:05 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+@cross_origin()
+def hello_world():
+    return 'Send requests to /bot to get responses'
+
+@app.route("/bot", methods=["POST", "GET"])
+def response():
+    query = dict(request.form)['query']
+    result = f"You have asked for: {query}"
+    return jsonify({"response" : result})
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port="5500")
